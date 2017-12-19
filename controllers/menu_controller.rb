33cf63913361c786d21b8e6a6337088e9e48d87d
Robert Marsh <nhe6dc@gmail.com>
2017-12-19 14:42:12 -0500

    cp4 challenge

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -14,7 +14,8 @@ class MenuController
     puts "2 - Create an entry"
     puts "3 - Search for an entry"
     puts "4 - Import entries from a CSV"
-    puts "5 - Exit"
+    puts "5 - View entry"
+    puts "6 - Exit"
     print "Enter your selection: "
 
     # #3
@@ -39,6 +40,10 @@ class MenuController
       read_csv
       main_menu
     when 5
+      system "clear"
+      entry_n_submenu
+      main_menu
+    when 6
       puts "Good-bye!"
       # #8
       exit(0)
@@ -47,6 +52,21 @@ class MenuController
       system "clear"
       puts "Sorry, that is not a valid input"
       main_menu
+    end
+  end
+
+  def entry_n_submenu
+    print "Entry number to view: "
+    selection = gets.chomp.to_i
+
+    if selection < @address_book.entries.count
+      puts @address_book.entries[selection]
+      puts "Press enter to return to the main menu"
+      gets.chomp
+      system "clear"
+    else
+      puts "#{selection} is not a valid input"
+      entry_n_submenu
     end
   end
 
